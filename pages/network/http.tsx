5cb418a6ce674f0ae965bb9f3ac85cbdb4cc43ae
Jonathan Rupp <jrupp@rightpoint.com>
2022-07-12 15:25:28 -0500

    Add simple pages that fetch data via HTTP and HTTPS

---
+++ pages/network/http.tsx
@@ -0,0 +1,18 @@
+function Page({ data }: {data: string}) {
+    return <div>
+        <h2>Data from google via HTTP</h2>
+        <div style={{ width: '80vw'}}><code>{data}</code></div>
+    </div>
+}
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`http://google.com`)
+    const data = await res.text()
+  
+    // Pass data to the page via props
+    return { props: { data } }
+  }
+  
+  export default Page
+  
